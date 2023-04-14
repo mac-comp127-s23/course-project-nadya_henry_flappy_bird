546bf33b4113f21781a7558d7f6fbeda92c3886e
Henry Heyden <henryden22@gmail.com>
2023-04-13 19:32:05 -0500

    Implement Points Graphic

--- src/flappybird/FlappyBird.java
+++ src/flappybird/FlappyBird.java
@@ -1,6 +1,7 @@
 package flappybird;
 
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsText;
 import edu.macalester.graphics.events.Key;
 
 public class FlappyBird {
@@ -10,12 +11,20 @@ public class FlappyBird {
 
     private CanvasWindow canvas;
     private int points;
+    private GraphicsText pointsText; //TODO add to UML doc
 
     private Bird bird;
     private PipesHandler pipesHandler;
 
     public FlappyBird() {
         canvas = new CanvasWindow("Flappy Bird", CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        //TODO most of the code below should be moved to a Reset Game function. :)
+
+        points = 0;
+        pointsText = new GraphicsText("" + points, 22, 45);
+        pointsText.setFontSize(40);
+        canvas.add(pointsText);
 
         bird = new Bird();
         canvas.add(bird.getGraphic());
@@ -31,13 +40,17 @@ public class FlappyBird {
         Runnable mainGameplayLoop = () -> {
             bird.move();
             if (pipesHandler.movePipes(bird)) points += 1;
-            System.out.println(points);
+            updatePointsText();
         };
         canvas.animate(mainGameplayLoop);
 
     }
 
     //TODO make a reset game function and think about what that needs to do
+
+    private void updatePointsText() { //TODO add to uml
+        pointsText.setText("" + points);
+    }
 
     public static void main(String[] args) {
         new FlappyBird();
